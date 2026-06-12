4acedef16045cc00f7e335461ea30b07a5ff0ab4
PeakyHell <contact@peakyhell.com>
2026-06-12 14:12:50 +0200

    Remove lazy.nvim for nvim.pack and add basic LSP config

--- init.lua
+++ init.lua
@@ -1,3 +1,22 @@
-require("config.lazy")
+-- Color Scheme --
+vim.pack.add{
+--  { src = "https://github.com/folke/tokyonight.nvim" },
+  { src = "https://github.com/catppuccin/nvim" },
+}
 
+vim.cmd.colorscheme "catppuccin-macchiato"
+
+
+-- LSPs --
+vim.pack.add{
+  { src = "https://github.com/neovim/nvim-lspconfig" },
+}
+
+vim.lsp.enable("pyright")
+
+
+-- Neovim Options
 require("config.options")
+
+
+-- vim: ts=2 sts=2 sw=2 et
