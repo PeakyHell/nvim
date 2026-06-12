7636d8d0f796d0c7a6838ba6f84c5eedebf521c6
PeakyHell <contact@peakyhell.com>
2026-06-12 14:39:05 +0200

    Add rust and c LSPs

--- init.lua
+++ init.lua
@@ -12,7 +12,9 @@ vim.pack.add{
   { src = "https://github.com/neovim/nvim-lspconfig" },
 }
 
-vim.lsp.enable("pyright")
+vim.lsp.enable("clangd") -- pacman -S clangd // brew install llvm
+vim.lsp.enable("pyright") -- npm i -g pyright
+vim.lsp.enable("rust_analyzer") -- rustup component add rust-src
 
 
 -- Neovim Options
